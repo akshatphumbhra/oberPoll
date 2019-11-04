fea5f5bd86764cdf723bf9cd889bef0a868e3c39
akshatphumbhra <43840267+akshatphumbhra@users.noreply.github.com>
2019-11-04 15:33:48 -0500

    Comments with info on data base creation

--- oberPoll.py
+++ oberPoll.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import *
+
+#SQLAlchemy- -Database
+#https://www.youtube.com/watch?v=QjtW-wnXlUY -- Tutorial
 
 app = Flask(__name__)
+#db=SQLAlchemy(app)
 
 @app.route('/')
 def index():
+    # id = db.Column('id', db.Integer, primary_key=True)
+    # name = db.Column('name', db.String)
+    # db.add({"name":"Akshat"})
+    # db.commit()
+    # print(db.query(1))
     return render_template("index.html")
 
 @app.route('/Akshat')
