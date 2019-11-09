e726e22fdc604733cd09df43051c25b730ec4167
JHSieber <48075418+JHSieber@users.noreply.github.com>
2019-11-09 08:36:41 -0500

    added comments and some more documentation for adatabases

--- oberPoll.py
+++ oberPoll.py
@@ -4,12 +4,18 @@ from flask import *
 #https://www.youtube.com/watch?v=QjtW-wnXlUY -- Tutorial
 
 app = Flask(__name__)
-#db=SQLAlchemy(app)
+#add a basedir that can be called 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'\
+#possibly change this to SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \'sqlite:///' + os.path.join(basedir, 'app.db')
+#and SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+db=SQLAlchemy(app) #creates database db
+
 
 @app.route('/')
-def index():
-    # id = db.Column('id', db.Integer, primary_key=True)
-    # name = db.Column('name', db.String)
+class index():
+     id = db.Column(db.Integer, primary_key=True, unique=true, nullable=true) #does true and false need to be True and False
+     name = db.Column(db.String(20), unique=true, nullable = false)
     # db.add({"name":"Akshat"})
     # db.commit()
     # print(db.query(1))
@@ -21,3 +27,43 @@ def akshat():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# -- ---
+# -- Globals
+# -- ---
+# 
+# -- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+# -- SET FOREIGN_KEY_CHECKS=0;
+# 
+# -- ---
+# -- Table 'User'
+# -- 
+# -- ---
+# 
+# DROP TABLE IF EXISTS `User`;
+# 		
+# CREATE TABLE `User` (
+#   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+#   `Name` VARCHAR(64) NULL DEFAULT NULL,
+#   `Paassword` VARCHAR(64) NULL DEFAULT NULL,
+#   `Student?` BINARY(8) NULL DEFAULT NULL,
+#   PRIMARY KEY (`id`)
+# );
+# 
+# -- ---
+# -- Foreign Keys 
+# -- ---
+# 
+# 
+# -- ---
+# -- Table Properties
+# -- ---
+# 
+# -- ALTER TABLE `User` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+# 
+# -- ---
+# -- Test Data
+# -- ---
+# 
+# -- INSERT INTO `User` (`id`,`Name`,`Paassword`,`Student?`) VALUES
+# -- ('','','','');
