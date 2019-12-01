8d0851f12da0dad3ebe9e165ef470c3b0db3fff0
akshatphumbhra <43840267+akshatphumbhra@users.noreply.github.com>
2019-12-01 15:14:24 -0500

    Working version

--- oberPoll.py
+++ oberPoll.py
@@ -2,6 +2,8 @@ from flask import *
 from models import db, Users, Polls, Topics, Options, UserPolls
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
+from datetime import datetime
+from config import SQLALCHEMY_DATABASE_URI
 
 oberPoll = Flask(__name__)
 
@@ -89,6 +91,108 @@ def createPoll():
 @oberPoll.route('/polls/<poll_name>')
 def poll():
     return render_template('index.html')
+
+@oberPoll.route('/api/poll/<poll_name>')
+def api_poll(poll_name):
+    poll = Topics.query.filter(Topics.title.like(poll_name)).first()
+
+    return jsonify({'Polls': [poll.to_json()]}) if poll else jsonify({'message': 'poll not found'})
+
+#Add and receive polls from or to the database
+@oberPoll.route('/api/polls', methods=['GET', 'POST'])
+def api_polls():
+
+    if request.method == 'POST':
+
+        #Get poll and save in the database
+        poll = request.get_json()
+
+        for key, value in poll.items():
+            if not value:
+                return jsonify({'message': 'value for {} is empty'.format(key)})
+
+        title = poll['title']
+        options_query = lambda option: Options.query.filter(Options.name.like(option))
+        options = [Polls(option=Options(name=option))
+                        if options_query(option).count() == 0
+                        else Polls(option=options_query(option).first()) for option in poll['options']]
+
+        #eta = datetime.utcfromtimestamp(poll['close_date'])
+        new_topic = Topics(title=title, options=options)#, close_date=eta)
+
+        db.session.add(new_topic)
+        db.session.commit()
+
+        #from tasks import close_poll
+
+        #close_poll.apply_async((new_topic.id, SQLALCHEMY_DATABASE_URI), eta=eta)
+        return jsonify({'message': 'Poll was successfully cretaed!'})
+
+    else:
+        #Query the database and return all polls as JSON objects
+        polls= Topics.query.join(Polls).order_by(Topics.id.desc()).all()
+        all_polls ={'Polls': [poll.to_json() for poll in polls]}
+
+
+        return jsonify(all_polls)
+
+@oberPoll.route('/api/polls/options')
+def api_polls_options():
+    all_options = [option.to_json() for option in Options.query.all()]
+
+    return jsonify(all_options)
+
+@oberPoll.route('/api/poll/vote', methods=['PATCH'])
+def api_poll_vote():
+    # poll = request.get_json()
+    #
+    # poll_title, option = (poll['poll_title'], poll['option'])
+    #
+    # join_tables = Polls.query.join(Topics).join(Options)
+    #
+    #  # Get topic and username from the database
+    # topic = Topics.query.filter_by(title=poll_title, status=True).first()
+    # user = Users.query.filter_by(username=session['user']).first()
+    # # if poll was closed in the background before user voted
+    # if not topic:
+    #     return jsonify({'message': 'Sorry! this poll has been closed'})
+    #
+    # # filter options
+    # option = join_tables.filter(Topics.title.like(poll_title), Topics.status == True).filter(Options.name.like(option)).first()
+    #
+    # # check if the user has voted on this poll
+    # poll_count = UserPolls.query.filter_by(topic_id=topic.id).filter_by(user_id=user.id).count()
+    # if poll_count > 0:
+    #     return jsonify({'message': 'Sorry! multiple votes are not allowed'})
+    #
+    # if option:
+    #     # record user and poll
+    #     user_poll = UserPolls(topic_id=topic.id, user_id=user.id)
+    #     db.session.add(user_poll)
+    #
+    #     # increment vote_count by 1 if the option was found
+    #     option.vote_count += 1
+    #     db.session.commit()
+    #
+    #     return jsonify({'message': 'Thank you for voting!'})
+    #
+    # return jsonify({'message': 'Option or poll was not found. Please try again'})
+    poll = request.get_json()
+
+    poll_title, option = (poll['poll_title'], poll['option'])
+
+    join_tables = Polls.query.join(Topics).join(Options)
+    # filter options
+    option = join_tables.filter(Topics.title.like(poll_title)).filter(Options.name.like(option)).first()
+
+    # increment vote_count by 1 if the option was found
+    if option:
+        option.vote_count += 1
+        db.session.commit()
+
+        return jsonify({'message': 'Thank you for voting'})
+
+    return jsonify({'message': 'option or poll was not found please try again'})
 
 if __name__ == "__main__":
     oberPoll.run()
